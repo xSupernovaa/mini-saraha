db32ef1bede9b3ee94c1bcd144ff68169e29088d
marioshafiek <58153069+marioshafiek@users.noreply.github.com>
2021-05-29 20:13:51 +0200

    Added function register and two functions of validation

--- mini-saraha/mini-saraha/Server.cpp
+++ mini-saraha/mini-saraha/Server.cpp
@@ -1,13 +1,15 @@
 #include "Server.h"
 #include <string>
-
+#include <regex>
+#include"FilesManager.h"
 using namespace std;
 
 vector<User> Server::users;
 int Server::userCount;
+FilesManager files;
 
 /**Add the reciver user to sender user's contact list*/
-void Server::addContact(int senderId, int recieverId)
+void addContact(int senderId, int recieverId)
 {
 	User sender = *Server::findUser(senderId);
 	if (Server::idExists(recieverId))
@@ -17,22 +19,22 @@ void Server::addContact(int senderId, int recieverId)
 }
 
 
-void Server::sendMessage(Message newMessage)
+void sendMessage(int senderId, int recieverId, string message)
 {
-	if (Server::idExists(newMessage.getSenderId()) && Server::idExists(newMessage.getRecieverId()))
+	if (Server::idExists(senderId) && Server::idExists(recieverId))
 	{
-		User reciever = *Server::findUser(newMessage.getRecieverId());
-		reciever.recieveMessage(newMessage);
+		User reciever = *Server::findUser(recieverId);
+		reciever.sendMassage(senderId, recieverId, message);
 	}
 	else
 		cout << "Sender or Reciver Id not found please check the ids and try again\n";
 }
-void Server::deleteLastMessage()
+void deleteLastMessage()
 {
 
 }
 
-void Server::viewMessages(int senderId)
+void viewMessages(int senderId)
 {
 	if (Server::idExists(senderId))
 	{
@@ -40,21 +42,79 @@ void Server::viewMessages(int senderId)
 	}
 }
 
-void Server::registerUser()
+void registerUser(string username, string password)
+{
+	//To check if username is already exist or dosen't exist
+	bool _username = false;
+	//To check if password match with pattern or not
+	bool _password = fasle;
+
+	_username = validate_username_register(username);
+	_password = validate_password_register(password);
+	//I do more than (else if) to print to the user the specific problem of registration 
+	//Username is incorrect 
+	if (_username == false || _password == true)
+	{
+		cout << "Username is already taken" << endl;
+	}
+	//password is incorrect
+	else if (_username == true || _password == false)
+	{
+		cout<<"Please enter a password with at least on upper case, one lower case, one digit, one special character, minimum eight in length"
+	}
+	//username and password is incorrect
+	else if (_username == false && _password == false)
+	{
+		cout << "Username and password incorrect" << endl;
+	}
+	if (username == true && password == true)
+	{	
+		User new_user(userCounter++, username, name, password);
+		files.add_new_user_instance_to_disc(new_user);
+		login(username, password);
+	}
+	
+	
+	
+}
+
+bool validate_password_registration(string password)
+{
+	//This regex will enforce at least on upper case, one lower case, one digit, one special character, minimum eight in length
+	regex pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+	if (regex_match(password, pattern))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool validate_username_register(string username)
+{
+	map<string, pair<string, int>> users = files.load_users_credintials_from_disc();
+	if (m1.count(username))
+	{
+		//username already exist
+		return false;
+	}
+	else
+	{
+		//username dosen't exist
+		return true;
+	}
+}
+
+void login(string username, string password)
 {
 
 }
-
-
-void Server::login()
+void saveSession()
 {
 
 }
-void Server::saveSession()
-{
-
-}
-void Server::loadSession()
+void loadSession()
 {
 
 }
@@ -63,4 +123,5 @@ Server::~Server()
 {
 
 }
+
 
